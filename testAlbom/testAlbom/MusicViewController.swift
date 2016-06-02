29624dac5f86990d2293d50b30dbc1dfb969ae5e
inna <inna.chernaya@hyuna.bb>
2016-06-02 12:29:38 +0300

    add player

--- testAlbom/testAlbom/MusicViewController.swift
+++ testAlbom/testAlbom/MusicViewController.swift
@@ -17,9 +17,42 @@ class MusicViewController: UIViewController {
     var arrList = [(String, NSURL)]()
     var compactConstraint: [NSLayoutConstraint] = []
     var audioPlayer = AVAudioPlayer()
+    var buttonPlay: UIButton!
+    var buttonPause: UIButton!
+    var butttonStop: UIButton!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        topBar = self.topLayoutGuide
+        
+        buttonPlay = UIButton(type: .Custom) as UIButton
+        buttonPlay.setImage(UIImage(named: "Play-44"), forState: .Normal)
+        buttonPlay.frame = CGRectMake(0, 0, 44, 44)
+        buttonPlay.addTarget(self, action: #selector(MusicViewController.play(_:)), forControlEvents: .TouchUpInside)
+        buttonPlay.enabled = false
+        buttonPlay.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(buttonPlay)
+    
+    
+        buttonPause = UIButton(type: .Custom) as UIButton
+        buttonPause.setImage(UIImage(named: "Pause-44"), forState: .Normal)
+        buttonPause.frame = CGRectMake(0, 0, 44, 44)
+        buttonPause.addTarget(self, action: #selector(MusicViewController.pause(_:)), forControlEvents: .TouchUpInside)
+        buttonPause.enabled = false
+        buttonPause.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(buttonPause)
+        
+        butttonStop = UIButton(type: .Custom) as UIButton
+        butttonStop.setImage(UIImage(named: "Stop-44"), forState: .Normal)
+        butttonStop.frame = CGRectMake(0, 0, 44, 44)
+        butttonStop.addTarget(self, action: #selector(MusicViewController.stop(_:)), forControlEvents: .TouchUpInside)
+        butttonStop.enabled = false
+        butttonStop.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(butttonStop)
+
         
         for aBook in arr {
             if let path = NSBundle.mainBundle().URLForResource(aBook, withExtension: "mp3", subdirectory: nil, localization: nil) {
@@ -50,13 +83,80 @@ class MusicViewController: UIViewController {
     func setLayout() {
         
         compactConstraint.append(NSLayoutConstraint(
+            item: buttonPlay,
+            attribute: NSLayoutAttribute.TopMargin,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: topBar,
+            attribute: NSLayoutAttribute.BottomMargin,
+            multiplier: 1.0,
+            constant: 20))
+        compactConstraint.append(NSLayoutConstraint(
+            item: tableView,
+            attribute: NSLayoutAttribute.Leading,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: view,
+            attribute: NSLayoutAttribute.Leading,
+            multiplier: 1.0,
+            constant: 10))
+        
+        compactConstraint.append(NSLayoutConstraint(
+            item: buttonPause,
+            attribute: NSLayoutAttribute.TopMargin,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: topBar,
+            attribute: NSLayoutAttribute.BottomMargin,
+            multiplier: 1.0,
+            constant: 20))
+        compactConstraint.append(NSLayoutConstraint(
+            item: buttonPause,
+            attribute: NSLayoutAttribute.CenterX,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: view,
+            attribute: NSLayoutAttribute.CenterX,
+            multiplier: 1.0,
+            constant: 0))
+        
+        compactConstraint.append(NSLayoutConstraint(
+            item: butttonStop,
+            attribute: NSLayoutAttribute.TopMargin,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: topBar,
+            attribute: NSLayoutAttribute.BottomMargin,
+            multiplier: 1.0,
+            constant: 20))
+        compactConstraint.append(NSLayoutConstraint(
+            item: butttonStop,
+            attribute: NSLayoutAttribute.TrailingMargin,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: view,
+            attribute: NSLayoutAttribute.TrailingMargin,
+            multiplier: 1.0,
+            constant: 10))
+        
+        compactConstraint.append(NSLayoutConstraint(
             item: tableView,
             attribute: NSLayoutAttribute.TopMargin,
             relatedBy: NSLayoutRelation.Equal,
-            toItem: view,
-            attribute: NSLayoutAttribute.TopMargin,
+            toItem: buttonPlay,
+            attribute: NSLayoutAttribute.BottomMargin,
             multiplier: 1.0,
-            constant: 0))
+            constant: 15))
+        compactConstraint.append(NSLayoutConstraint(
+            item: tableView,
+            attribute: NSLayoutAttribute.TopMargin,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: butttonStop,
+            attribute: NSLayoutAttribute.BottomMargin,
+            multiplier: 1.0,
+            constant: 15))
+        compactConstraint.append(NSLayoutConstraint(
+            item: tableView,
+            attribute: NSLayoutAttribute.TopMargin,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: buttonPause,
+            attribute: NSLayoutAttribute.BottomMargin,
+            multiplier: 1.0,
+            constant: 15))
         compactConstraint.append(NSLayoutConstraint(
             item: tableView,
             attribute: NSLayoutAttribute.BottomMargin,
@@ -74,7 +174,38 @@ class MusicViewController: UIViewController {
             multiplier: 1.0,
             constant: 0))
     }
-  
+    
+    func play(sender: UIButton) {
+        butttonStop.enabled = true
+        buttonPause.enabled = true
+        buttonPlay.enabled = false
+        dispatch_async(dispatch_get_main_queue(), {
+            self.audioPlayer.prepareToPlay()
+            self.audioPlayer.play()
+            print("play")})
+        }
+
+    
+    func stop(sender: UIButton) {
+        butttonStop.enabled = false
+        buttonPause.enabled = false
+        buttonPlay.enabled = true
+        audioPlayer.stop()
+        print(audioPlayer.currentTime)
+        audioPlayer.currentTime = 0
+        print("stop")
+    }
+    
+    
+    func pause(sender: UIButton) {
+        butttonStop.enabled = true
+        buttonPause.enabled = false
+        buttonPlay.enabled = true
+        audioPlayer.pause()
+        print("pause")
+    }
+    
+ 
 }
 
 extension MusicViewController: UITableViewDelegate {
@@ -93,18 +224,17 @@ extension MusicViewController: UITableViewDataSource {
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
        
+        buttonPlay.enabled = true
+        
          do {
             try audioPlayer = AVAudioPlayer(contentsOfURL: arrList[indexPath.row].1)
         } catch {
             let fetchError = error as NSError
             print(fetchError)
-        }      
-      
-        dispatch_async(dispatch_get_main_queue(), {
-            self.audioPlayer.prepareToPlay()
-            self.audioPlayer.play()
-              print("play")})
+        }
+            audioPlayer.delegate = self
     }
+
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
        return arrList.count
@@ -114,4 +244,27 @@ extension MusicViewController: UITableViewDataSource {
         return 1
     }
     
+}
+
+extension MusicViewController: AVAudioPlayerDelegate {
+    
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully
+        flag: Bool) {
+        
+        buttonPlay.enabled = true
+        buttonPause.enabled = false
+        butttonStop.enabled = false
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(player: AVAudioPlayer,
+                                        error: NSError?) {
+    }
+    
+    func audioPlayerBeginInterruption(player: AVAudioPlayer) {
+    }
+    
+    func audioPlayerEndInterruption(player: AVAudioPlayer) {
+        
+       
+    }
 }
